0df4993f65646391ca75c781b2a91f1357771ae3
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-04 16:59:11 +0100

    add handlers package

--- src/main/java/org/example/dataanalyticskafka/handlers/PageEventHandler.java
+++ src/main/java/org/example/dataanalyticskafka/handlers/PageEventHandler.java
@@ -1,4 +1,19 @@
 package org.example.dataanalyticskafka.handlers;
 
+import org.example.dataanalyticskafka.events.PageEvent;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+
+@Component
 public class PageEventHandler {
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer(){
+        return (input)->{
+            System.out.println("************");
+            System.out.println(input.toString());
+            System.out.println("************");
+        };
+    }
 }
