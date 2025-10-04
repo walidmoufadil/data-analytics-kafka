80a3c9ea49df38ea55a10123c6ae6fc37d8ca3b0
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-04 17:08:06 +0100

    update controllers

--- src/main/java/org/example/dataanalyticskafka/controllers/PageEventController.java
+++ src/main/java/org/example/dataanalyticskafka/controllers/PageEventController.java
@@ -2,16 +2,30 @@ package org.example.dataanalyticskafka.controllers;
 
 
 import org.example.dataanalyticskafka.events.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyWindowStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.binder.kafka.streams.InteractiveQueryService;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 @RestController
 public class PageEventController {
+    @Autowired
+    private InteractiveQueryService interactiveQueryService;
     private StreamBridge streamBridge;
 
     public PageEventController(StreamBridge streamBridge) {
@@ -22,6 +36,23 @@ public class PageEventController {
         PageEvent event = new PageEvent(name, Math.random()>0.5?"U1":"U2", new Date(), 10+new Random().nextInt(1000));
         streamBridge.send(topic, event);
         return event;
+    }
+
+    @GetMapping(path = "/analytics",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Map<String, Long>> analytics(){
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence->{
+                    Map<String,Long> stringLongMap=new HashMap<>();
+                    ReadOnlyWindowStore<String, Long> windowStore = interactiveQueryService.getQueryableStore("count-store", QueryableStoreTypes.windowStore());
+                    Instant now=Instant.now();
+                    Instant from=now.minusMillis(5000);
+                    KeyValueIterator<Windowed<String>, Long> fetchAll = windowStore.fetchAll(from, now);
+                    while (fetchAll.hasNext()){
+                        KeyValue<Windowed<String>, Long> next = fetchAll.next();
+                        stringLongMap.put(next.key.key(),next.value);
+                    }
+                    return stringLongMap;
+                });
     }
 
 }
