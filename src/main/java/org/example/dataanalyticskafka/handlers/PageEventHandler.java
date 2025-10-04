d8e5bafbc994c7a732c9751316deca20357ef531
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-10-04 17:07:52 +0100

    update controllers

--- src/main/java/org/example/dataanalyticskafka/handlers/PageEventHandler.java
+++ src/main/java/org/example/dataanalyticskafka/handlers/PageEventHandler.java
@@ -1,11 +1,21 @@
 package org.example.dataanalyticskafka.handlers;
 
 import org.example.dataanalyticskafka.events.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+import java.util.Date;
+import java.util.Random;
 import java.util.function.Consumer;
-
+import java.util.function.Function;
+import java.util.function.Supplier;
 @Component
 public class PageEventHandler {
     @Bean
@@ -15,5 +25,31 @@ public class PageEventHandler {
             System.out.println(input.toString());
             System.out.println("************");
         };
+    }
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return ()->{
+            return new PageEvent(
+                    Math.random()>0.5?"P1":"P2",
+                    Math.random()>0.5?"U1":"U2",
+                    new Date(),
+                    10+new Random().nextInt(10000)
+            );
+        };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStream(){
+        return (stream)->
+                stream
+                        .filter((k,v)->v.duration()>100)
+                        .map((k,v)->new KeyValue<>(v.name(), 0L))
+                        .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                        .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                        .count(Materialized.as("count-store"))
+                        .toStream()
+                        .map((k,v)->new KeyValue<>(k.key(), v))
+                ;
+
     }
 }
